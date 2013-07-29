8da673269647fbc4523a75b1065d199774fb3522
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 20:52:20 +0900

    7.2 oneSpare - make it pass

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,8 +10,14 @@ public class Game {
         int score = 0;
         int i = 0;
         for(int frame = 0; frame < 10; frame++) {
-            score += rolls[i] + rolls[i + 1];
-            i += 2;
+            if(rolls[i] + rolls[i + 1] == 10) { // spare
+                score += 10 + rolls[i + 2];
+                i += 2;
+            }
+            else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
         }
         return score;
     }
