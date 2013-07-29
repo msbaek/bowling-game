cb687ab6fc440853c57930d6977736f4b9d1e4cf
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 21:00:25 +0900

    8.1 oneStrike - make it pass

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,7 +10,11 @@ public class Game {
         int score = 0;
         int firstFrame = 0;
         for(int frame = 0; frame < 10; frame++) {
-            if(isSpare(firstFrame)) {
+            if(rolls[firstFrame] == 10) { // strike
+                score += 10 + rolls[firstFrame + 1] + rolls[firstFrame + 2];
+                firstFrame += 1;
+            }
+            else if(isSpare(firstFrame)) {
                 score += 10 + rolls[firstFrame + 2];
                 firstFrame += 2;
             }
