d7ecbffa7ede5272784c1989536c90a21b26086e
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 20:40:08 +0900

    5.3 oneSpare - refactoring
    - 이제 테스트가 수행되니 리팩토링하자.
    - Roll을 배열에 저장하자.
    - 이에 misplaced responsibility가 해소되었다.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,15 @@
 public class Game {
-    private Integer score = 0;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public Integer score() {
+        int score = 0;
+        for(int i = 0; i < rolls.length; i++)
+            score += rolls[i];
         return score;
     }
 }
