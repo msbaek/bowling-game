82726b4f8b887f6a8c51a307bae3e21401bd5259
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 20:32:49 +0900

    4.3.1 allOnes - extract method

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -22,16 +22,18 @@ public class GameTest {
     public void gutterGame() {
         int rolls = 20;
         int pins = 0;
-        for(int i = 0; i < rolls; i++) {
-            game.roll(pins);
-        }
+        rollMany(rolls, pins);
         assertThat(game.score(), is(0));
+    }
+
+    private void rollMany(int rolls, int pins) {
+        for(int i = 0; i < rolls; i++)
+            game.roll(pins);
     }
 
     @Test
     public void allOnes() {
-        for(int i = 0; i < 20; i++)
-            game.roll(1);
+        rollMany(20, 1);
         assertThat(game.score(), is(20));
     }
 }
