adb99e257fbfd6bb9434b6a2a5477fee8aa12dd7
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 20:53:31 +0900

    7.3 oneSpare - refactoring
    - rename i to firstFrame
    - 처음부터 firstFrame이라는 변수명을 찾은 것이 아니라 여러차례의 시도로 찾음

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,15 +8,15 @@ public class Game {
 
     public Integer score() {
         int score = 0;
-        int i = 0;
+        int firstFrame = 0;
         for(int frame = 0; frame < 10; frame++) {
-            if(rolls[i] + rolls[i + 1] == 10) { // spare
-                score += 10 + rolls[i + 2];
-                i += 2;
+            if(rolls[firstFrame] + rolls[firstFrame + 1] == 10) { // spare
+                score += 10 + rolls[firstFrame + 2];
+                firstFrame += 2;
             }
             else {
-                score += rolls[i] + rolls[i + 1];
-                i += 2;
+                score += rolls[firstFrame] + rolls[firstFrame + 1];
+                firstFrame += 2;
             }
         }
         return score;
