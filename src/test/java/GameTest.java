edd0d7b17c0c7f60fbea28b9c20fb1d636efa147
Myeongseok Baek <msbaek@daumcorp.com>
2013-07-29 20:29:50 +0900

    4.1 allOnes - add failing test
    - next most simple and interesting test case

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -24,4 +24,11 @@ public class GameTest {
             game.roll(0);
         assertThat(game.score(), is(0));
     }
+
+    @Test
+    public void allOnes() {
+        for(int i = 0; i < 20; i++)
+            game.roll(1);
+        assertThat(game.score(), is(20));
+    }
 }
